5e1c2aa56382f2e5daf71becedbd92e04637ad73
ffarias <ffarias@intecsoft.com.ar>
2022-09-15 12:03:48 -0300

    Flor - AlertScreen v.1 funcionando

--- lib/screens/alert.dart
+++ lib/screens/alert.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_dos/theme/app_theme.dart';
 
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
   
+  void displayDialogo(BuildContext context){
+    showDialog(
+      barrierDismissible: false,
+      context: context, 
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Presionaste la alerta, ¿felíz?'),
+          shape: RoundedRectangleBorder(
+           borderRadius: BorderRadius.circular(10)
+      ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            
+            children: const [
+              Text('Este es el contenido de la Alerta, triste como este sitio.'),
+              SizedBox(height: 10),
+              Icon(Icons.heart_broken_rounded, color: AppTheme.iconColors, size: 50)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),     
+              child: const Text('Cancelar') 
+              )
+          ],
+        );
+      }
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       body: Center(
-         child: Text('AlertScreen'),
+         child: Padding(
+           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+           child: ElevatedButton(
+            onPressed:() => displayDialogo(context),
+            child: const Text('Mostrar alerta', style: TextStyle(fontSize: 15)),
+           ),
+         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.close),
+        onPressed: () => Navigator.pop(context)        
       ),
     );
   }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -3,14 +3,31 @@ import 'package:flutter/material.dart';
 class AppTheme {
 
   static const Color primary = Color.fromARGB(255, 2, 51, 73);
+
   static const Color iconColors = Colors.blueGrey;
+
   static final ThemeData lightTheme = 
     ThemeData.light().copyWith(
         primaryColor: primary,
+
+
         appBarTheme: const AppBarTheme(
           color: primary,
           elevation: 0
-        )
-  );
+        ),
 
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: primary
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color.fromARGB(255, 5, 50, 73),
+            shape: const StadiumBorder(),
+            elevation: 0
+
+          ),
+        )
+      
+  );
 }
